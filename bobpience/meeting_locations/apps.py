8059a64d1e1feed315541639ba25e1ad1302eff9
NohSungwoo <shtjddn0817@gmail.com>
2024-08-13 13:15:04 +0900

    Chore: app 기능 추가

---
+++ bobpience/meeting_locations/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MeetingLocationsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "meeting_locations"
---
+++ bobpience/taste_tests/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TasteTestsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "taste_tests"

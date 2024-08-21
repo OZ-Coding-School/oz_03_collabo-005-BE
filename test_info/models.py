aeb2304ab1d4aab3de1abdb8629fd3e64346a2cc
NohSungwoo <shtjddn0817@gmail.com>
2024-08-21 11:20:25 +0900

    Chore: 누락 사항 반영

--- test_info/models.py
+++ test_info/models.py
@@ -7,7 +7,7 @@ from common.models import CommonModel
 class TasteTestQuestion(models.Model):
     taste_question = models.TextField()
     taste_question_category = models.CharField(max_length=50)
-    taste_question_image = models.ImageField()
+    taste_question_image = models.ImageField(default="abc.jpg")
 
     def __str__(self):
         return self.taste_question
@@ -18,11 +18,12 @@ class TasteTestAnswer(models.Model):
     taste_question = models.ForeignKey(
         "test_info.TasteTestQuestion", on_delete=models.CASCADE
     )
-    taste_answer_category = models.CharField(max_length=100)
+    taste_answer = models.CharField(max_length=100)
+    taste_score = models.PositiveIntegerField(max_length=6)
 
 
 # FTI 검사 질문
-class FtiTestQuestion(models.Model):
+class FTITestQuestion(models.Model):
     fti_question = models.TextField()
     fti_question_image = models.ImageField()
 
@@ -31,7 +32,7 @@ class FtiTestQuestion(models.Model):
 
 
 # FTI 검사 답변
-class FtiTestAnswer(models.Model):
+class FTITestAnswer(models.Model):
     fti_question = models.ForeignKey(
         "test_info.FtiTestQuestion", on_delete=models.CASCADE
     )
@@ -40,7 +41,7 @@ class FtiTestAnswer(models.Model):
 
 
 # 비로그인 유저 검사 결과
-class NonUserFTITestResult(CommonModel):
+class FTITestResult(CommonModel):
     uuid = models.CharField(max_length=100)
     fti_type = models.ForeignKey(
         "categories.FTIType",

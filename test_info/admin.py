ccedc3a5b37dc9827a27d93cb4761a1cfabd09c7
NohSungwoo <shtjddn0817@gmail.com>
2024-08-21 19:11:40 +0900

    Feat: FTITestResult, admin 등록

--- test_info/admin.py
+++ test_info/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import FTITestAnswer, FTITestQuestion, TasteTestAnswer, TasteTestQuestion
+from .models import FTITestAnswer, FTITestQuestion, TasteTestAnswer, TasteTestQuestion, FTITestResult
 
 
 @admin.register(FTITestQuestion)
@@ -10,6 +10,11 @@ class FTITestQuestionAdmin(admin.ModelAdmin):
 
 @admin.register(FTITestAnswer)
 class FTITestAnswerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FTITestResult)
+class FTITestResultAdmin(admin.ModelAdmin):
     pass
 
 

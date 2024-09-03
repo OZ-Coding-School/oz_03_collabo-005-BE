2e1db97a6083741b4672490981a77379abd55694
Hyunminmax <hyunmin.shin81@gmail.com>
2024-09-03 12:55:10 +0900

    Feat/Categories -  Categories load - FTIType - MeetingAgeGroup - MeetingGender - MeetingPaymentMethod - Review - TimeSort

--- categories/urls.py
+++ categories/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from .views import FoodFilterList
+from .views import *
 
 urlpatterns = [
     # Food Filter
     path("foodfilter/", FoodFilterList.as_view(), name="FoodFilterList"),
+    path("ftitypefiler/", FTITypeList.as_view(), name="FTITypeList"),
+    path("locationfilter/", LocationList.as_view(), name="LocationList"),
+    path("meetingagefilter/", MeetingAgeGroupList.as_view(), name="MeetingAgeGroupList"),
+    path("meetinggenderfilter/", MeetingGenderGroupList.as_view(), name="MeetingGenderGroupList"),
+    path("meetingpaymentfilter/", MeetingPaymentMethodList.as_view(), name="MeetingPaymentMethodList"),
+    path("reviewfilter/", ReviewFilterList.as_view(), name="ReviewFilterList"),
+    path("timesortfilter/", TimeSortFilterList.as_view(), name="TimeSortFilterList"),
 ]
--- categories/views.py
+++ categories/views.py
@@ -3,17 +3,95 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import FoodFilter
-from .serializers import CategoryFoodFilterSerializer
-
+from .models import *
+from .serializers import *
 
 class FoodFilterList(APIView):
     serializer_class = CategoryFoodFilterSerializer
 
-    @extend_schema(tags=["Foods"])
+    @extend_schema(tags=["Category"])
     def get(self, request):
         # 필터 전부 로드
         categoryFilter = FoodFilter.objects.all()
 
         serializer = self.serializer_class(instance=categoryFilter, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class FTITypeList(APIView):
+    serializer_class = FTITypeSerializer
+
+    @extend_schema(tags=["Category"])
+    def get(self, request):
+        # 필터 전부 로드
+        categoryFilter = FTIType.objects.all()
+
+        serializer = self.serializer_class(instance=categoryFilter, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class LocationList(APIView):
+    serializer_class = LocationSerializer
+
+    @extend_schema(tags=["Category"])
+    def get(self, request):
+        # 필터 전부 로드
+        categoryFilter = Location.objects.all()
+
+        serializer = self.serializer_class(instance=categoryFilter, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class MeetingAgeGroupList(APIView):
+    serializer_class = MeetingAgeGroupSerializer
+
+    @extend_schema(tags=["Category"])
+    def get(self, request):
+        # 필터 전부 로드
+        categoryFilter = MeetingAgeGroup.objects.all()
+
+        serializer = self.serializer_class(instance=categoryFilter, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class MeetingGenderGroupList(APIView):
+    serializer_class = MeetingGenderGroupSerializer
+
+    @extend_schema(tags=["Category"])
+    def get(self, request):
+        # 필터 전부 로드
+        categoryFilter = MeetingGenderGroup.objects.all()
+
+        serializer = self.serializer_class(instance=categoryFilter, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class MeetingPaymentMethodList(APIView):
+    serializer_class = MeetingPaymentMethodSerializer
+
+    @extend_schema(tags=["Category"])
+    def get(self, request):
+        # 필터 전부 로드
+        categoryFilter = MeetingPaymentMethod.objects.all()
+
+        serializer = self.serializer_class(instance=categoryFilter, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class ReviewFilterList(APIView):
+    serializer_class = ReviewCategorySerializer
+
+    @extend_schema(tags=["Category"])
+    def get(self, request):
+        # 필터 전부 로드
+        categoryFilter = ReviewCategory.objects.all()
+
+        serializer = self.serializer_class(instance=categoryFilter, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class TimeSortFilterList(APIView):
+    serializer_class = TimeCategorySerializer
+
+    @extend_schema(tags=["Category"])
+    def get(self, request):
+        # 필터 전부 로드
+        categoryFilter = TimeSortCategory.objects.all()
+
+        serializer = self.serializer_class(instance=categoryFilter, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
